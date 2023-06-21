e584a4689b9d823d3a67de74f79cdb499f58a353
theazasoo <45148332+theazasoo@users.noreply.github.com>
2023-06-21 20:32:27 +0100

    MQTT Sample Project Added
    MQTT sample project added to the folder

--- ESP32/Evios_On_Air/src/buffer.c
+++ ESP32/Evios_On_Air/src/buffer.c
@@ -1,89 +1,36 @@
-//void app_main() {}
-
-
-//EVIOS State Machine
-
-#include <stdio.h>
-#include <stdbool.h>
 #include <stdint.h>
+#include "esp_system.h"
 
-typedef enum {
-  sysIddle,
-  bluetoothMsgTx,
-  MQTTMsgTx,
-  ConnectivityErr
-} EVIOS_states_t;
+typedef struct mqtt_message_buffer {
+  uint8_t *payload;
+  size_t payload_size;
+  bool is_free;
+} mqtt_message_buffer_t;
 
-
-void EVIOS_StateMachineInit(); //initialises the system
-
-
-
-void EVIOS_StateMachine(EVIOS_state_t *state) {
-  switch (*state) {
-    case SysIddle:
-      if (CheckConnectivity()) {
-        printf("Check connectivity\n");
-        state = bluetoothMsgRx;
-      } else {
-        state = ConnectivityErr;
-      }
-      break;
-    case bluetoothMsgTx:
-      printf("Send message over Bluetooth\n");
-//1. Buffer is checked to be ready and consistent
-//2. check for paired device
-//3. check for connectivity
-//4. transmit to the device
-//5. full bufer is transmited 
-
-      state = SysIddle;
-      break;
-    case MQTTMsgTx:
-      printf("Send message over MQTT\n");
-      state = SysIddle;
-      break;
-    case ConnectivityErr:
-      printf("Retry sending message\n");
-      state = SysIddle;
-      break;
+mqtt_message_buffer_t *mqtt_message_buffer_alloc(size_t payload_size) {
+  mqtt_message_buffer_t *buffer = (mqtt_message_buffer_t *)heap_caps_malloc(
+      sizeof(mqtt_message_buffer_t), MALLOC_CAP_8BIT);
+  if (buffer == NULL) {
+    return NULL;
   }
-}
 
-bool bluetoothRx()
-{
-
-}
-bool MQTTRx()
-{
-  return true;
-  
-  //1. mqtt message is received fully
-  //2. message is stored in a buffer for bluetooth tx
-  //3. ....
-}
-
-int main() {
-  EVIOS_StateMachineInit();
-  EVIOS_state_t activeState;
-  while (1)
-  {
-      if(bluetoothRx())
-      {
-        activeState = MQTTMsgTx;
-        EVIOS_StateMachine(&activeState);
-      }else if(MQTTRx()){
-        activeState = bluetoothMsgTx;
-        EVIOS_StateMachine(&activeState);
-      }
-
+  buffer->payload = (uint8_t *)heap_caps_malloc(payload_size, MALLOC_CAP_8BIT);
+  if (buffer->payload == NULL) {
+    free(buffer);
+    return NULL;
   }
-  return 0;
+
+  buffer->payload_size = payload_size;
+  buffer->is_free = false;
+
+  return buffer;
 }
 
-/*
-Initialise the state the state machine
+void mqtt_message_buffer_free(mqtt_message_buffer_t *buffer) {
+  if (buffer == NULL) {
+    return;
+  }
 
-
-*/
-
+  heap_caps_free(buffer->payload);
+  heap_caps_free(buffer);
+}
